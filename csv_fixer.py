1dbd41f9baaf97a1db3920259ac111ad057e8e2d
Nigel <s2876731@PC029262.staff.ad.griffith.edu.au>
2018-07-13 17:31:40 +1000

    Add csv fixer

---
+++ csv_fixer.py
@@ -0,0 +1,32 @@
+import csv
+from itertools import islice
+
+for month in range(0, 12):
+    month = month + 1
+    path = "D:/AIRtravel/New Monthly/New 2017/griffith_n_2017m0" + str(month) + ".csv"
+    out = "D:/AIRtravel/New Monthly/New 2017/griffith_nn_2017m0" + str(month) + ".csv"
+    print(path)
+    filereader = csv.reader(
+        open(path, mode='r+', encoding="utf-8"), delimiter=',')
+    newline = []
+    next(filereader)
+    for line in filereader:
+        if line[2] == line[3]:
+            line[2] = ''
+        if len(line[2]) == 0:
+            line[1] = ''
+
+        line.insert(0, 2017)
+        line.insert(1, month)
+        line.insert(6, '')
+        line.insert(7, '')
+        line.insert(8, '')
+        line.pop()
+        line.pop()
+
+        newline.append(line)
+
+    with open(out, 'w', newline='') as csv_file:
+        writer = csv.writer(csv_file, delimiter=',')
+        for writeline in newline:
+            writer.writerow(writeline)
