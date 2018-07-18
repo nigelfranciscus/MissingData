4d79588bdfcbd058b34744c93384358115480e64
Nigel <s2876731@PC029262.staff.ad.griffith.edu.au>
2018-07-18 17:13:08 +1000

    latest

--- airport_info.py
+++ airport_info.py
@@ -1,3 +1,4 @@
+from __future__ import division
 import csv
 from math import cos, asin, sqrt
 
@@ -12,30 +13,60 @@ def distance(lat1, lon1, lat2, lon2):
 # Read the airport location data
 airport_reader = csv.reader(
     open("C:/Users/s2876731.STAFF/Desktop/NewCO2/airports_preprocess.csv", mode='r+', encoding="utf-8"), delimiter=',')
+# Build key-value for IATA code and lat-long pair
 airport_dict = {each[4]: each[2:4] for each in airport_reader}
 
 # read the brisbane airport information
-read_brisbane = csv.reader(open('C:/Users/s2876731.STAFF/Desktop/Flights/FlightData_AKL_2017-01.csv', mode='r+'),
-                           delimiter=',')
-brisbane_location = [row[2:6] for row in read_brisbane]
+# read_brisbane = csv.reader(open('C:/Users/s2876731.STAFF/Desktop/Flights/FlightData_AKL_2017-01.csv', mode='r+'),
+#                            delimiter=',')
+# brisbane_location = [row[2:6] for row in read_brisbane]
+# print(brisbane_location)
+
+# lat_long = {}
+# for each_location in brisbane_location:
+#     first = airport_dict.get(each_location[0])
+#     second = airport_dict.get(each_location[1])
+#     third = airport_dict.get(each_location[2])
+#     fourth = airport_dict.get(each_location[3])
+#
+#     lat_long[each_location[0], each_location[1], each_location[2], each_location[3]] = first, second, third, fourth
+#
+# # for key, value in lat_long.items():
+# #     for each_value in value:
+# #         if each_value is not None:
+# #             print(each_value)
+#
+#
+# with open('C:/Users/s2876731.STAFF/Desktop/Flights/akl_viz.csv', 'w', newline='') as csv_file:
+#     writer = csv.writer(csv_file, delimiter=',')
+#     for key, value in lat_long.items():
+#         writer.writerow([key[0], key[1], key[2], key[3], value[0], value[1], value[2], value[3]])
+
+read_top400 = csv.reader(open('Top400carbonroutes.csv', mode='r+'), delimiter=',')
+next(read_top400)
 
 lat_long = {}
-for each_location in brisbane_location:
-    first = airport_dict.get(each_location[0])
-    second = airport_dict.get(each_location[1])
-    third = airport_dict.get(each_location[2])
-    fourth = airport_dict.get(each_location[3])
+lower = 0.5
+upper = 5
+minimum = 6.976
+maximum = 39.704
+for each_data in read_top400:
+    first = airport_dict.get(each_data[0])
+    second = airport_dict.get(each_data[1])
+    ton = float(each_data[3])/1000000
+    # normalization
+    lineThickness = lower + (ton - minimum)*(upper-lower)/(maximum-minimum)
 
-    lat_long[each_location[0], each_location[1], each_location[2], each_location[3]] = first, second, third, fourth
+    lat_long[each_data[0], each_data[1], round(lineThickness, 3)] = first, second
 
-# for key, value in lat_long.items():
-#     for each_value in value:
-#         if each_value is not None:
-#             print(each_value)
+for key, value in lat_long.items():
+    print("{" + "origin:{ latitude: " + value[0][0] + ", longitude: " + value[0][1] + "}, destination: { latitude: " +
+           value[1][0] + ", longitude: " + value[1][1] + "}, strokeWidth: " + str(key[2]) +
+           " strokeColor: 'red', arcSharpness: 0.1 },")
 
 
-with open('C:/Users/s2876731.STAFF/Desktop/Flights/akl_viz.csv', 'w', newline='') as csv_file:
-    writer = csv.writer(csv_file, delimiter=',')
-    for key, value in lat_long.items():
-        writer.writerow([key[0], key[1], key[2], key[3], value[0], value[1], value[2], value[3]])
 
+# with open('C:/Users/s2876731.STAFF/Desktop/Flights/akl_viz.csv', 'w', newline='') as csv_file:
+#     writer = csv.writer(csv_file, delimiter=',')
+#     for key, value in lat_long.items():
+#         writer.writerow([key[0], key[1], key[2], key[3], value[0], value[1], value[2], value[3]])
--- csv_fixer.py
+++ csv_fixer.py
@@ -1,6 +1,7 @@
 import csv
 from itertools import islice
 
+# iterate for 12 months
 for month in range(0, 12):
     month = month + 1
     path = "D:/AIRtravel/New Monthly/New 2017/griffith_n_2017m0" + str(month) + ".csv"
@@ -11,6 +12,7 @@ for month in range(0, 12):
     newline = []
     next(filereader)
     for line in filereader:
+        # New Amadeus Format (Why do they change?)
         if line[2] == line[3]:
             line[2] = ''
         if len(line[2]) == 0:
